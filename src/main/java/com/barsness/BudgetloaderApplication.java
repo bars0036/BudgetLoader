90e43cbf7a0f3b22ddaa96fbd4a99094a84ec8c3
matt.barsness <matt.barsness@bluestembrands.com>
2017-10-05 15:57:06 -0500

    initial commit

---
+++ src/main/java/com/barsness/BudgetloaderApplication.java
@@ -0,0 +1,12 @@
+package com.barsness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetloaderApplication.class, args);
+	}
+}
