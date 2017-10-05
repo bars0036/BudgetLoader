1af64c865c21057c6918f894201df300b8592244
matt.barsness <matt.barsness@bluestembrands.com>
2017-10-05 17:22:34 -0500

    test written

--- src/test/java/com/barsness/LoaderTests.java
+++ src/test/java/com/barsness/LoaderTests.java
@@ -7,8 +7,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 /**
  * Created by matt.barsness on 10/5/17.
@@ -23,6 +30,16 @@ public class LoaderTests {
     @Test
     public void testTransactionLoader(){
         ResponseEntity<TransactionLoadResponse> forEntity = restTemplate.getForEntity("/load/transactions?fileName={fileName}", TransactionLoadResponse.class, "test.csv");
+        TransactionLoadResponse response = forEntity.getBody();
+        Assert.assertEquals(0, response.getTransLoaded());
+
+    }
+
+    @Test
+    public void testTransactionLoaderWithFile(){
+        LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+        map.add("file", new FileSystemResource("./src/main/resources/Transactions.csv"));
+        ResponseEntity<TransactionLoadResponse> forEntity = restTemplate.exchange("/load/transactions", HttpMethod.POST, new HttpEntity<MultiValueMap<String, Object>>(map), TransactionLoadResponse.class);
         TransactionLoadResponse response = forEntity.getBody();
         Assert.assertEquals(0, response.getTransLoaded());
 
